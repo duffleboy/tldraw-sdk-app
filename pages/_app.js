d305fa0003a0be9549555a6384bb5e485a4b9515
duffleboy <duffleboy@gmail.com>
2025-05-12 07:50:51 +0100

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+import 'https://cdn.jsdelivr.net/npm/@tldraw/tldraw@1.4.0/dist/tldraw.css';
+import '../styles/globals.css'
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
